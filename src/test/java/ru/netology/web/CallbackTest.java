f27264e7cb3613dfb819276cd68b5e62d9ae4683
PetrIvChe <cherniavskiipetr@gmail.com>
2021-11-26 12:09:49 +0300

    Test

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -3,8 +3,15 @@ package ru.netology.web;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CallbackTest {
     private WebDriver driver;
@@ -25,5 +32,22 @@ public class CallbackTest {
         driver.quit();
         driver = null;
     }
+
+    @Test
+    public void shouldReturnForm() {
+        driver.get("http://localhost:9999");
+        System.out.println("");
+//        driver.findElement().sendKeys("Васильев Василий");
+//        driver.findElement().sendKeys("+75145684789");
+        List<WebElement> textFields = driver.findElements(By.className("input__control"));
+        textFields.get(0).sendKeys("Васильев Василий");
+        textFields.get(1).sendKeys("+75145684789");
+        driver.findElement(By.className("checkbox_box")).click();
+        driver.findElement(By.tagName("button")).click();
+        String actualText = driver.findElement(By.className("alert-success")).getText();
+        String expectedText = "Ваша заявкауспешно отправлена! Наш менеджер свяжится с вами в ближайшее время";
+        assertEquals(actualText, expectedText, "Текст сообщения не совпадает");
+    }
+
 
 }
